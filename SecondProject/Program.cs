cc395632f2806a544223e681f5f7f230289e98ee
Tuomas Reijonen <hture01@xamk.fi>
2023-09-18 15:04:30 +0300

    Add project files.

---
+++ SecondProject/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä x");
+
+            var x = Console.ReadLine();
+
+            Console.WriteLine("Syötä y");
+
+            var y = Console.ReadLine();
+
+            Console.WriteLine("Minkä operaation haluat suorittaa: ");
+            Console.WriteLine("Saatavilla olevat operaatiot: summa, jako, miinus, kerto");
+
+            //kerto = *
+            //plussa = +
+            //miinus = -
+            //jako = /
+            var operaatio = Console.ReadLine();
+
+            int result = 0;
+
+            if (operaatio == "summa")
+            {
+                result = int.Parse(x) + int.Parse(y);
+            }
+            else if(operaatio == "miinus")
+            {
+                result = int.Parse(x) - int.Parse(y);
+            }
+            else if (operaatio == "jako")
+            {
+                result = int.Parse(x) / int.Parse(y);
+            }
+            else if (operaatio == "kerto")
+            {
+                result = int.Parse(x) * int.Parse(y);
+            }
+
+            Console.WriteLine($"Interpolation example: Result {x} {operaatio} {y} = {result}");
+        }
+    }
+}
